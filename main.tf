c321e45a5cfad55487d0b9f0ef6b0296be6dc9ef
mateuszjedynak44 <mateuszjedynak332@gmail.com>
2026-06-11 10:11:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,9 @@ resource "azurerm_public_ip" "pip" {
   name                = "pip-server"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+
+  allocation_method = "Static"
+  sku               = "Standard"
 }
 
 resource "azurerm_network_security_group" "nsg" {
